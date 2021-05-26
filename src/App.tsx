b81730c381ae07905a489fb478b8fbc3cd0ad231
Rafael Brito <rafael.souzabrito7@gmail.com>
2021-05-25 22:37:07 -0300

    finished dtmoney ignite project

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Header } from './components/Header';
 import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from './styles/global';
+import { TransactionsProvider } from './hooks/useTransactions';
 
 Modal.setAppElement('#root');
 
@@ -21,7 +22,7 @@ export function App() {
   }
   
   return (
-    <>
+    <TransactionsProvider>
       <Header 
       onOpenNewTransactionModal={handleOpenNewTransactionModal}
       />
@@ -31,6 +32,6 @@ export function App() {
       onRequestClose={handleCloseNewTransactionModal}
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
